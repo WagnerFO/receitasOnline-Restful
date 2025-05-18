d0143f8499ef1da163a79da7377e0f90dbc42c1f
WagnerFO <wagnerf.oliveira@unifacol.edu.br>
2025-05-18 10:40:44 -0300

    add repository

--- demo/demo/src/main/java/com/example/demo/repository/AvaliacaoRepository.java
+++ demo/demo/src/main/java/com/example/demo/repository/AvaliacaoRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.UserEntity;
+import com.example.demo.entity.AvaliacaoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface AvaliacaoRepository extends JpaRepository<AvaliacaoEntity, Long> {
 }
--- demo/demo/src/main/java/com/example/demo/repository/ReceitaRepository.java
+++ demo/demo/src/main/java/com/example/demo/repository/ReceitaRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.UserEntity;
+import com.example.demo.entity.ReceitaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface ReceitaRepository extends JpaRepository<ReceitaEntity, Long> {
+
 }
